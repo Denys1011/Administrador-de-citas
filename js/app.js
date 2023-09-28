b9d552025bfde417af92dca55a936440411e9637
Denys Lugo <denyslugolnn@gmail.com>
2023-09-27 21:18:18 -0400

    Eliminar y modificar citas agregado

--- js/app.js
+++ js/app.js
@@ -154,7 +154,8 @@ class UI {
 
                 // Añade un botón de editar...
                 const btnEditar = document.createElement('button');
-                btnEditar.onclick = () => cargarEdicion(cursor.value);
+                const cita = cursor.value;
+                btnEditar.onclick = () => cargarEdicion(cita);
 
                 btnEditar.classList.add('btn', 'btn-info');
                 btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
@@ -214,11 +215,20 @@ function nuevaCita(e) {
         // Estamos editando
         administrarCitas.editarCita( {...citaObj} );
 
+        const transaction = DB.transaction(["citas"], "readwrite");
+        const objectStore = transaction.objectStore("citas");
+
+        objectStore.put(citaObj);
+
+        transaction.oncomplete = () => {
         ui.imprimirAlerta('Guardado Correctamente');
 
         formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
 
         editando = false;
+        }
+
+        
 
     } else {
         // Nuevo Registrando
@@ -269,9 +279,22 @@ function reiniciarObjeto() {
 
 
 function eliminarCita(id) {
-    administrarCitas.eliminarCita(id);
+    
+    const transaction = DB.transaction(["citas"], "readwrite");
+    const objectStore = transaction.objectStore("citas");
+
+    objectStore.delete(id)
 
     ui.imprimirCitas(administrarCitas)
+
+    transaction.oncomplete = () => {
+        console.log(`Cita ${id} eliminada...`);
+        iu.imprimirCitas();
+    }
+
+    transaction.onerror = () => {
+        console.log("hubo un error");
+    }
 }
 
 function cargarEdicion(cita) {
